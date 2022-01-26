05885fb9a88a1fb31d13ae7f7598e10f9619beb8
Diego Ruiz Guiza <Diego Ruiz>
2022-01-25 21:37:34 -0500

    se agregan audios en español, ajuste en los textos de cada pais, se agrega boton para reproducir audios

--- app/src/main/java/com/udistrital/geographyplay/pais2.java
+++ app/src/main/java/com/udistrital/geographyplay/pais2.java
@@ -1,19 +1,49 @@
 package com.udistrital.geographyplay;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
-import androidx.fragment.app.Fragment;
+import java.util.Locale;
 
 
 public class pais2 extends Fragment {
     @Nullable
+    View view;
+    MediaPlayer mp;
+    ImageButton ibArg;
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.pais2_layout,container,false);
+        view = inflater.inflate(R.layout.pais2_layout,container,false);
+        ibArg = (ImageButton) view.findViewById(R.id.ibtnArg);
+        if(Locale.getDefault().toString().equals("es_CO")){
+            mp = MediaPlayer.create(getContext(), R.raw.argentina);
+        }
+
+        ibArg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.start();
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (this.isVisible()) {
+            if (!isVisibleToUser) {
+                Log.d("MyFragment", "Not visible anymore. Stopping audio.");
+                mp.pause();
+            }
+        }
     }
 }
