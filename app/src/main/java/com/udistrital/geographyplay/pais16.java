324f4225b749349e872597bd5ef1068b8b2e3872
Diego Ruiz Guiza <Diego Ruiz>
2022-01-26 18:10:27 -0500

    Se ajustan layout de paises se agrega boton de audio en español

--- app/src/main/java/com/udistrital/geographyplay/pais16.java
+++ app/src/main/java/com/udistrital/geographyplay/pais16.java
@@ -1,17 +1,52 @@
 package com.udistrital.geographyplay;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import java.util.Locale;
 
 
 public class pais16 extends Fragment {
     @Nullable
+
+    View view;
+    MediaPlayer mp;
+    ImageButton ibUsa;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.pais16_layout,container,false);
+        view = inflater.inflate(R.layout.pais16_layout,container,false);
+        ibUsa = (ImageButton) view.findViewById(R.id.ibtnUsa);
+        if(Locale.getDefault().toString().equals("es_CO")){
+            mp = MediaPlayer.create(getContext(), R.raw.estadosunidos);
+        }
+
+        ibUsa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.start();
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (this.isVisible()) {
+            if (!isVisibleToUser) {
+                Log.d("MyFragment", "Not visible anymore. Stopping audio.");
+                mp.pause();
+            }
+        }
     }
 }
--- app/src/main/java/com/udistrital/geographyplay/pais34.java
+++ app/src/main/java/com/udistrital/geographyplay/pais34.java
@@ -1,17 +1,52 @@
 package com.udistrital.geographyplay;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import java.util.Locale;
 
 
 public class pais34 extends Fragment {
     @Nullable
+
+    View view;
+    MediaPlayer mp;
+    ImageButton ibUru;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.pais34_layout,container,false);
+        view = inflater.inflate(R.layout.pais34_layout,container,false);
+        ibUru = (ImageButton) view.findViewById(R.id.ibtnUruguay);
+        if(Locale.getDefault().toString().equals("es_CO")){
+            mp = MediaPlayer.create(getContext(), R.raw.uruguay);
+        }
+
+        ibUru.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.start();
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (this.isVisible()) {
+            if (!isVisibleToUser) {
+                Log.d("MyFragment", "Not visible anymore. Stopping audio.");
+                mp.pause();
+            }
+        }
     }
 }
