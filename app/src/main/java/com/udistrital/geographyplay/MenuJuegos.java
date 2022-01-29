6fcb05743e3533a7a62730b1e81d93102c1f7b48
Carlos Javier Suarez <csuarego@emeal.nttdata.com>
2022-01-29 09:32:11 -0500

    Se ajusta menu

--- app/src/main/java/com/udistrital/geographyplay/MenuJuegos.java
+++ app/src/main/java/com/udistrital/geographyplay/MenuJuegos.java
@@ -4,27 +4,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
-
-import com.udistrital.geographyplay.R;
 import com.udistrital.geographyplay.databinding.FragmentMenuJuegosBinding;
 
 public class MenuJuegos extends Fragment {
 
     private FragmentMenuJuegosBinding binding;
     View vista;
-    Button memorama;
-    Button puzle;
-    Button preguntas;
-    Button online;
+    CardView cardPuzzle;
+    CardView cardConcen;
+    CardView cardSaber;
+    CardView cardOnline;
     private Context mContext;
     @Override
     public void onAttach(Context context) {
@@ -41,32 +36,32 @@ public class MenuJuegos extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         vista = inflater.inflate(R.layout.fragment_menu_juegos, container, false);
-        memorama=(Button)  vista.findViewById(R.id.btnConcentrese);
-        online =(Button)  vista.findViewById(R.id.btnOnline);
-        memorama.setOnClickListener(new View.OnClickListener() {
+        cardPuzzle = vista.findViewById(R.id.cardPuzzle);
+        cardConcen = vista.findViewById(R.id.cardConcen);
+        cardSaber = vista.findViewById(R.id.cardSaber);
+        cardOnline = vista.findViewById(R.id.cardOnline);
+        cardPuzzle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),SelectPuzle.class);
+                startActivity(intent);
+            }
+        });
+        cardConcen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getActivity(),SelectConcentrese.class);
                 startActivity(intent);
             }
         });
-        puzle=(Button)  vista.findViewById(R.id.btnPuzle);
-        puzle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(),SelectPuzle.class);
-                startActivity(intent);
-            }
-        });
-        preguntas=(Button)  vista.findViewById(R.id.btnPreguntas);
-        preguntas.setOnClickListener(new View.OnClickListener() {
+        cardSaber.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getActivity(),Conocimientos.class);
                 startActivity(intent);
             }
         });
-        online.setOnClickListener(new View.OnClickListener() {
+        cardOnline.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(),Conexion.class);
